78349bc9dd72574fe11643e527582de9ca9dcfa5
KSchlendrian <cbii@hotmail.de>
2023-11-13 10:34:32 +0100

    final

---
+++ main.js
@@ -0,0 +1,132 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.getElementById('01');
+  let audio = new Audio('audio/c.mp3');
+
+  if (div) {
+    div.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.getElementById('02');
+  let audio = new Audio('audio/d.mp3');
+
+  if (div) {
+    div.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.getElementById('03');
+  let audio = new Audio('audio/e.mp3');
+
+  if (div) {
+    div.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.getElementById('04');
+  let audio = new Audio('audio/cis.mp3');
+
+  if (div) {
+    div.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.getElementById('05');
+  let audio = new Audio('audio/dis.mp3');
+
+  if (div) {
+    div.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.getElementById('06');
+  let audio = new Audio('audio/f.mp3');
+
+  if (div) {
+    div.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.getElementById('07');
+  let audio = new Audio('audio/g.mp3');
+
+  if (div) {
+    div.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.getElementById('08');
+  let audio = new Audio('audio/a.mp3');
+
+  if (div) {
+    div.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.getElementById('09');
+  let audio = new Audio('audio/h.mp3');
+
+  if (div) {
+    div.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.getElementById('10');
+  let audio = new Audio('audio/fis.mp3');
+
+  if (div) {
+    div.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.getElementById('11');
+  let audio = new Audio('audio/gis.mp3');
+
+  if (div) {
+    div.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.getElementById('12');
+  let audio = new Audio('audio/ais.mp3');
+
+  if (div) {
+    div.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+});
