a746f887e313e4fd0eff9c3b18d8c9d170b3865e
KSchlendrian <cbii@hotmail.de>
2023-11-13 11:25:55 +0100

    final4

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  function addClickListener(noteId, audioFileName) {
-    let div = document.getElementById(noteId);
-    let audio = new Audio(`audio/${audioFileName}.mp3`);
+  function addClickListener(element) {
+    let audio = new Audio(`audio/${element.dataset.audio}.mp3`);
 
-    if (div) {
-      div.addEventListener('click', () => {
-        audio.currentTime = 0;
-        audio.play();
-      });
-    }
+    element.addEventListener('click', () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
   }
-
-  addClickListener('01', 'c');
-  addClickListener('02', 'd');
-  addClickListener('03', 'e');
-  addClickListener('04', 'cis');
-  addClickListener('05', 'dis');
-  addClickListener('06', 'f');
-  addClickListener('07', 'g');
-  addClickListener('08', 'a');
-  addClickListener('09', 'h');
-  addClickListener('10', 'fis');
-  addClickListener('11', 'gis');
-  addClickListener('12', 'ais');
+  document.querySelectorAll('.piano-key').forEach(addClickListener);
 });
