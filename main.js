227902eb31b85e3c76b21c8832bd0d8ee9a5ae85
KSchlendrian <cbii@hotmail.de>
2023-11-13 14:10:05 +0100

    final6

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   document.querySelectorAll('.piano-key').forEach(addClickListener);
 });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.querySelectorAll('.piano-key').forEach((element) => {
+//     element.addEventListener('click', playByClick);
+//   });
+// });
+
+// function playByClick(event) {
+//   const eventTarget = event.target.closest('.piano-key');
+
+//   if ('audio' in element.dataset && element.dataset.audio.length > 0) {
+//     let audio = new Audio(`audio/{$element.dataset.audio}.mp3`);
+
+//     audio.currentTime = 0;
+//     audio.play();
+//   }
+// }
